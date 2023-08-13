3a0f74336da4976599ec2b78cdd3fbd7e6020004
Viktor Stepanenko <vstepanenko@gmail.com>
2023-08-13 21:27:32 +0200

    Add dynamodb to the hit counter construct

--- cdk_workshop/hitcounter.py
+++ cdk_workshop/hitcounter.py
@@ -1,9 +1,27 @@
 from constructs import Construct
 from aws_cdk import aws_lambda as _lambda
+from aws_cdk import aws_dynamodb as ddb
 
 class HitCounter(Construct):
+
+    @property
+    def handler(self):
+        return self._handler
+    
+
     def __init__(self, scope: Construct, id: str,
                  downstream: _lambda.IFunction, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        #TODO
+        table = ddb.Table(
+            self,
+            partition_key = {'name': 'path', 'type': ddb.AttributeType.STRING}
+        )
+
+        self._handler = _lambda.Function(
+            self, 'HitCountHandler',
+            runtime = _lambda.Runtime.PYTHON_3_7,
+            handler='hitcount.handler',
+            environment={'DOWNSTREAM_FUNCTION_NAME': downstream.fuction_name,
+                         'HITS_TABLE_NAME': table.table_name}
+        )
