ddbc90ed3446b895eceb8ac3e6732d2d8197105d
Viktor Stepanenko <vstepanenko@gmail.com>
2023-08-13 21:53:22 +0200

    Fix bug

--- cdk_workshop/hitcounter.py
+++ cdk_workshop/hitcounter.py
@@ -21,7 +21,8 @@ class HitCounter(Construct):
         self._handler = _lambda.Function(
             self, 'HitCountHandler',
             runtime = _lambda.Runtime.PYTHON_3_7,
-            handler='hitcount.handler',
-            environment={'DOWNSTREAM_FUNCTION_NAME': downstream.function_name,
+            handler = 'hitcount.handler',
+            code = _lambda.Code.from_asset('lambda'),
+            environment = {'DOWNSTREAM_FUNCTION_NAME': downstream.function_name,
                          'HITS_TABLE_NAME': table.table_name}
         )
