05c801d2ca40f7433bd2d048203d5339b974779d
Viktor Stepanenko <vstepanenko@gmail.com>
2023-08-13 20:12:08 +0200

    Initial commit

--- cdk_workshop/cdk_workshop_stack.py
+++ cdk_workshop/cdk_workshop_stack.py
@@ -1,26 +1,19 @@
 from constructs import Construct
-from aws_cdk import (
-    Duration,
-    Stack,
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
-)
-
+from aws_cdk import Stack
+from aws_cdk import aws_lambda as _lambda
+from aws_cdk import aws_apigateway as apigw
 
 class CdkWorkshopStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, **kwargs):
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "CdkWorkshopQueue",
-            visibility_timeout=Duration.seconds(300),
+        # defines an AWS Lambda resource
+        my_lambda = _lambda.Function(
+            self, "HelloHandler",
+            runtime = _lambda.Runtime.PYTHON_3_7,
+            code = _lambda.Code.from_asset('lambda'),
+            handler = 'hello.handler',
         )
 
-        topic = sns.Topic(
-            self, "CdkWorkshopTopic"
-        )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
+        apigw.LambdaRestApi(self, "Endpoint", handler=my_lambda)
---
+++ lambda/hello.py
@@ -0,0 +1,10 @@
+import json
+
+def handler(event, context):
+    print('request: {}'.format(json.dumps(event)))
+    output = {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'text/plain'},
+        'body': 'Good Night, CDK! You have hit {}\n'.format(event['path'])
+    }
+    return output
